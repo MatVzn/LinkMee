9c527e87d19070531bf110282c9421d49d952c7c
MatVzn <matteovoleite@gmail.com>
2024-02-21 00:54:01 -0300

    updated js, html and css

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-function redMode() {
-  const html = document.documentElement
-  if(html.classList.contains('red')) {
-    html.classList.remove('red')
-  } else {
-    html.classList.remove('green', 'red', 'blue')
-    html.classList.add('red')
-  }
-}
+const html = document.documentElement
 
-function greenMode() {
-  const html = document.documentElement
-  if(html.classList.contains('green')) {
-    html.classList.remove('green')
-  } else {
-    html.classList.remove('green', 'red', 'blue')
-    html.classList.add('green')
-  }
-}
+function changeTheme(color) {
 
-function blueMode() {
-  const html = document.documentElement
-  if(html.classList.contains('blue')) {
-    html.classList.remove('blue')
+  if (html.classList.contains(color)) {
+
+    html.className = ''
+
   } else {
-    html.classList.remove('green', 'red', 'blue')
-    html.classList.add('blue')
+
+    html.className = ''
+    html.classList.add(color)
+
   }
 }
